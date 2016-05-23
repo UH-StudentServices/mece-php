8a47799127d79a00879cfea3fbcd3b2a58470f5a
Mikael Kundert <mikael.kundert@wunderkraut.com>
2016-05-23 11:54:38 +0300

    Use proper autoloader

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,8 +3,5 @@
  * @license GPL, or GNU General Public License, version 3
  * @license http://opensource.org/licenses/GPL-3.0
  * @see README.md how to contribute to this project
- * @todo USE PROPER AUTOLOADER
  */
-require_once __DIR__ . '/MECE/Message.php';
-require_once __DIR__ . '/MECE/MultilingualStringValue.php';
-require_once __DIR__ . '/MECE/Notification.php';
+require_once __DIR__ . '/../vendor/autoload.php';
