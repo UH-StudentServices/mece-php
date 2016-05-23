c0fb0d2a50f35b6905ff965f96bdf3a602988fdb
Mikael Kundert <mikael.kundert@wunderkraut.com>
2016-05-23 10:14:40 +0300

    Added phpunit configuraiton and bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @license GPL, or GNU General Public License, version 3
+ * @license http://opensource.org/licenses/GPL-3.0
+ * @see README.md how to contribute to this project
+ * @todo USE PROPER AUTOLOADER
+ */
+require_once __DIR__ . '/src/Message.php';
+require_once __DIR__ . '/src/MultilingualStringValue.php';
